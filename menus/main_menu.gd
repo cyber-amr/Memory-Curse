37c95309bf7ae9d105ac7aa1fb119660153f7a3f
Amr <amr@programmer.net>
2024-11-11 15:37:21 +0300

    add `main_menu`

---
+++ menus/main_menu.gd
@@ -0,0 +1,13 @@
+extends Node
+
+
+func _on_start() -> void:
+	pass # Replace with function body.
+
+
+func _on_options() -> void:
+	pass # Replace with function body.
+
+
+func _on_quit() -> void:
+	get_tree().quit()
