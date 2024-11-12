a1a2bd2e7a455a39767a97e77a43368db272c376
Amr <amr@programmer.net>
2024-11-13 02:29:49 +0300

    fix a typo

--- manager.gd
+++ manager.gd
@@ -3,6 +3,7 @@ extends Node
 
 func new_unit(id: String) -> Unit:
 	var file = id + ".tscn"
-	var scene : PackedScene = load("res://units/nodes/" + id)
+	var scene : PackedScene = load("res://units/nodes/" + file)
+	if !scene: push_error("Could not load scene (%s)" % [file]); return null
 	var node : Unit = scene.instantiate()
 	return node
