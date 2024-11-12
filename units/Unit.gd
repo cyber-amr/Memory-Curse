6a9d73e12de941bc5e17b0336d6b7491a02c9983
Amr <amr@programmer.net>
2024-11-12 18:52:32 +0300

    change the way of handling mesh and collision sapes

--- units/Unit.gd
+++ units/Unit.gd
@@ -2,21 +2,8 @@ class_name Unit
 extends CharacterBody3D
 
 
-@export var shape : Shape3D
-@export var mesh : Mesh
+@export var shape : CollisionShape3D
+@export var mesh : MeshInstance3D
 
 @export var cost : int = 100
 @export var health : int = 100
-
-
-func _ready() -> void:
-	if shape:
-		var col_node := CollisionShape3D.new()
-		col_node.shape = shape
-		add_child(col_node)
-	
-	if mesh:
-		var mesh_node := MeshInstance3D.new()
-		mesh_node.mesh = mesh
-		add_child(mesh_node)
-	
