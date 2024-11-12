4eaef0f7c5c16e322768abc4254c50d4125461d6
Amr <amr@programmer.net>
2024-11-12 04:01:41 +0300

    add static types

--- units/Unit.gd
+++ units/Unit.gd
@@ -11,12 +11,12 @@ extends CharacterBody3D
 
 func _ready() -> void:
 	if shape:
-		var col_node = CollisionShape3D.new()
+		var col_node := CollisionShape3D.new()
 		col_node.shape = shape
 		add_child(col_node)
 	
 	if mesh:
-		var mesh_node = MeshInstance3D.new()
+		var mesh_node := MeshInstance3D.new()
 		mesh_node.mesh = mesh
 		add_child(mesh_node)
 	
