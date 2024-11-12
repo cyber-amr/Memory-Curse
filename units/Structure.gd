a17b2560319e10d13da06ba9942bfde28a11cccb
Amr <amr@programmer.net>
2024-11-12 19:02:49 +0300

    change the way to handle bounds

--- units/Structure.gd
+++ units/Structure.gd
@@ -2,15 +2,4 @@ class_name Structure
 extends Unit
 
 
-@export var bounds : Shape3D
-
-var bounds_area : Area3D
-
-
-func _ready() -> void:
-	if bounds:
-		bounds_area = Area3D.new()
-		var bounds_col_node := CollisionShape3D.new()
-		bounds_col_node.shape = bounds
-		bounds_area.add_child(bounds_col_node)
-		add_child(bounds_area)
+@export var bounds_area : Area3D
