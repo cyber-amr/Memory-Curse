c6946f720bdbd615a44b0b87f43375cb8276e51c
Amr <amr@programmer.net>
2024-11-14 01:57:34 +0300

    add option for render resolution

--- menus/options_menu.gd
+++ menus/options_menu.gd
@@ -1,6 +1,21 @@
 extends Node
 
 
+func _ready() -> void:
+	update()
+	Manager.options_updated.connect(update)
+
+
 func back():
 	Manager.save_options()
 	queue_free()
+
+
+func update():
+	# Video
+	$"%ResolutionSlider".value = Manager.options["resolution"]
+
+
+func change_resolution(value: float) -> void:
+	Manager.options["resolution"] = value
+	Manager.options_updated.emit()
