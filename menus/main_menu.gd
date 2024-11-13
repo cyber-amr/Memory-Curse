65355e7cb39ecefe8975b350f7f54ebea9b62e50
Amr <amr@programmer.net>
2024-11-14 02:32:01 +0300

    quit on `Esc` pressed

--- menus/main_menu.gd
+++ menus/main_menu.gd
@@ -5,6 +5,12 @@ var game_scene := load("res://world/game.tscn")
 var options_menu : PackedScene = load("res://menus/options_menu.tscn")
 
 
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_cancel"):
+		get_viewport().set_input_as_handled()
+		_on_quit()
+
+
 func _on_start() -> void:
 	get_tree().change_scene_to_packed(game_scene)
 
